49c133bd27daf989e5c6803a05592c45e56982a0
Aghula <agnieszkaryzner2@gmail.com>
2019-12-14 15:58:24 +0100

    seventh commit

--- src/test/java/com/agaryzner/HelloServiceTest.java
+++ src/test/java/com/agaryzner/HelloServiceTest.java
@@ -3,25 +3,40 @@ package com.agaryzner;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloServiceTest {
-
-    private HelloService System_Under_Test = new HelloService();
+    private static final String WELCOME = "Hello";
 
     @Test
     @DisplayName("test prepare greeting null, returns greeting with fallback value")
 
     public void testPrepareGreetingWithoutName() {
-        var result = System_Under_Test.prepareGreeting(null);
-        assertEquals("Hello " + HelloService.FALLBACK_NAME + "!", result);
+
+        var mockRepository = alwaysReturningHelloRepository();
+        var System_Under_Test = new HelloService(mockRepository);
+        var result = System_Under_Test.prepareGreeting(null,"-1");
+        assertEquals(WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
     }
 
     @Test
     @DisplayName("test prepare greeting with name, returns greeting with value")
     public void testPrepareGreetingWithName() {
-        var name = "test";
-        var result = System_Under_Test.prepareGreeting(name);
-        assertEquals("Hello " + name + "!", result);
+        var mockRepository = alwaysReturningHelloRepository();
+        var System_Under_Test = new HelloService(mockRepository);
+        String name = "test";
+        var result = System_Under_Test.prepareGreeting(name,"-1");
+        assertEquals(WELCOME + " " + name + "!", result);
+    }
+    private LanguageRepository alwaysReturningHelloRepository(){
+        return new LanguageRepository() {
+            @Override
+            Optional<Language> findById (Long id) {
+                return Optional.of(new Language(null,WELCOME, null));
+            }
+        };
     }
 }
